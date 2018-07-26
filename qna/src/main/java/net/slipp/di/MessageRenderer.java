7ae8a4445faeea0e7c07441cd013531261b16ec3
DaeHoan Shin <xbb123@naver.com>
2018-07-27 01:04:15 +0900

    ClassPathXmlApplicationContext 작업

--- qna/src/main/java/net/slipp/di/MessageRenderer.java
+++ qna/src/main/java/net/slipp/di/MessageRenderer.java
@@ -1,5 +1,8 @@
 package net.slipp.di;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class MessageRenderer {
 	private MessageProvider messageProvider;
 	
@@ -11,11 +14,8 @@ public class MessageRenderer {
 		System.out.println(messageProvider.getMessage());
 	}
 	public static void main(String[] args) {
-		MessageRenderer renderer = new MessageRenderer();
-		renderer.setMessageProvider(new HelloWorldMessageProvider());
-		renderer.render();
-		
-		renderer.setMessageProvider(new HiWorldMessageProvider());
+		ApplicationContext ac = new ClassPathXmlApplicationContext("di.xml");
+		MessageRenderer renderer = (MessageRenderer) ac.getBean("messageRenderer");
 		renderer.render();
 	}
 	
