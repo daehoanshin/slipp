12bca784ce6b5b2753ab6bced5175e0aef93dadf
DaeHoan Shin <xbb123@naver.com>
2018-09-03 01:45:20 +0900

    component-scan complete

--- qna/src/main/java/net/slipp/dao/users/MyBatisUserDao.java
+++ qna/src/main/java/net/slipp/dao/users/MyBatisUserDao.java
@@ -1,37 +1,21 @@
 package net.slipp.dao.users;
 
-import javax.annotation.PostConstruct;
-import javax.sql.DataSource;
+import javax.annotation.Resource;
 
 import org.apache.ibatis.session.SqlSession;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.stereotype.Repository;
 
 import net.slipp.domain.users.User;
 
+@Repository("userDao")
 public class MyBatisUserDao implements UserDao {
+	@Resource(name="sqlSession")
 	private SqlSession sqlSession;
-	private DataSource dataSource;
-	
-	private static final Logger log = LoggerFactory.getLogger(MyBatisUserDao.class);
-	@PostConstruct
-	public void initialize() {
-		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-		populator.addScript(new ClassPathResource("slipp.sql"));
-		DatabasePopulatorUtils.execute(populator, dataSource);
-		log.info("database initialized success!");
-	}
 	
 	public void setSqlSession(SqlSession sqlSession) {
 		this.sqlSession = sqlSession;
 	}
 	
-	public void setDataSource(DataSource dataSource) {
-		this.dataSource = dataSource;
-	}
 	@Override
 	public User findById(String userId) {
 		return sqlSession.selectOne("UserMapper.findById", userId);
